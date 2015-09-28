a00aa761b11a617db30a651d1bccf7190c0d518c
Nuno Carvalho <mestre.smash@gmail.com>
2015-09-28 12:13:07 +0100

    Read config from file

--- druid-api.js
+++ druid-api.js
@@ -1,6 +1,14 @@
 
 var config = require('./config');
+    path   = require('path'),
     server = require('./lib/server');
+
+// read config from file if available
+if (process.argv.length == 3) {
+  var file = process.argv[2];
+  console.log('Reading config from: '+file);
+  config = require(path.resolve(file));
+}
 
 // start server
 server.listen(config.server.port, config.server.listen,  function () {
